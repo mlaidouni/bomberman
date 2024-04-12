381658c90ccdddd5e7968dc72de235659a3cdabf
khemakhe <ayman.khemakhem@etu.u-paris.fr>
2024-04-12 18:31:51 +0200

    Serveur simple

--- src-srv/server.c
+++ src-srv/server.c
@@ -1,3 +1,69 @@
-int main(){
-	return 0;
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define SIZE_MESS 100
+#define NOM "Cerise"
+
+void affiche_connexion(struct sockaddr_in6 adrclient){
+  char adr_buf[INET6_ADDRSTRLEN];
+  memset(adr_buf, 0, sizeof(adr_buf));
+  
+  inet_ntop(AF_INET6, &(adrclient.sin6_addr), adr_buf, sizeof(adr_buf));
+  printf("adresse client : IP: %s port: %d\n", adr_buf, ntohs(adrclient.sin6_port));
+}
+
+int main(int argc, char** args) {
+
+  //*** creation de la socket serveur ***
+  int sock = socket(PF_INET6, SOCK_STREAM, 0);
+  if(sock < 0){
+    perror("creation socket");
+    exit(1);
+  }
+
+  //*** creation de l'adresse du destinataire (serveur) ***
+  struct sockaddr_in6 address_sock; //= {AF_INET6,  htons(atoi(args[1])), 0, IN6ADDR_ANY_INIT, 0};
+  memset(&address_sock, 0, sizeof(address_sock));
+  address_sock.sin6_family = AF_INET6;
+  address_sock.sin6_port = htons(7878);
+  address_sock.sin6_addr=in6addr_any;
+  
+  //*** on lie la socket au port PORT ***
+  int r = bind(sock, (struct sockaddr *) &address_sock, sizeof(address_sock));
+  if (r < 0) {
+    perror("erreur bind");
+    exit(2);
+  }
+
+  //*** Le serveur est pret a ecouter les connexions sur le port PORT ***
+  r = listen(sock, 0);
+  if (r < 0) {
+    perror("erreur listen");
+    exit(2);
+  }
+
+  //*** le serveur accepte une connexion et cree la socket de communication avec le client ***
+  struct sockaddr_in6 adrclient;
+  memset(&adrclient, 0, sizeof(adrclient));
+  socklen_t size=sizeof(adrclient);
+  int sockclient = accept(sock, (struct sockaddr *) &adrclient, &size);
+  if(sockclient == -1){
+    perror("probleme socket client");
+    exit(1);
+  }	   
+
+  affiche_connexion(adrclient);
+  
+  //*** fermeture socket client ***
+  close(sockclient);
+
+  //*** fermeture socket serveur ***
+  close(sock);
+  
+  return 0;
 }
